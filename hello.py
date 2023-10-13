65a5745afbe9c4a73601500100895478624495e3
Daichi HOZAKI <t21cs045@yamanashi.ac.jp>
2023-10-13 15:06:14 +0900

    Hello, Python world!

---
+++ hello.py
@@ -0,0 +1,15 @@
+'''
+Created on 2023/10/13
+
+@author: t21cs045
+'''
+print("Hello, Python world!")
+a = 10
+b = 0.000001
+c='string'
+d = 10; e = 0.000001; f="string"
+# This is comment
+print(a,b,c)
+print(d,e,f)
+for x in {1,2,3} :
+    print(x)
